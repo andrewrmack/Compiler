cd2c075f18f357e1e3bb59bac409ed91c048c3a3
Andrew Mack <andrewrmack@gmail.com>
2018-02-10 14:41:10 -0600

    Test intermediate stages

--- test/extrinsic-tests.sh
+++ test/extrinsic-tests.sh
@@ -3,6 +3,8 @@
 for fn in `ls test/baselines/*.in`;
 do
   infile=$(basename $fn)
+  lexfile=test/baselines/${infile%.*}.lex
+  parsefile=test/baselines/${infile%.*}.parse
   outfile=test/baselines/${infile%.*}.out
 
   echo -n "testing '"
@@ -11,7 +13,9 @@ do
 
   if [ -e $outfile ]
   then
-      diff <(stack exec compiler \-\- $fn) $outfile || exit 1
+      diff <(stack exec compiler \-\- --lex $fn) $lexfile || (>&2 echo "Failed lex"; exit 1)
+      diff <(stack exec compiler \-\- --parse $fn) $fn || (>&2 echo "Failed parse"; exit 1)
+      diff <(stack exec compiler \-\- $fn) $outfile || (>&2 echo "Failed evaluation"; exit 1)
   else
       >&2 echo "No such file '${outfile}'"
       exit 1;
