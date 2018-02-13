b67cd093c61a76d6de4b4c6851dcca6a1a38bc23
Andrew Mack <andrewrmack@gmail.com>
2018-02-12 20:43:58 -0600

    Improve tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,5 +3,6 @@ import System.Process (system)
 
 main :: IO ()
 main = do
+  putStrLn "" -- Fix weird formatting
   exitcode <- system "./test/extrinsic-tests.sh"
   exitWith exitcode
--- test/extrinsic-tests.sh
+++ test/extrinsic-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo -e "Testing normal inputs:\n"
 for fn in `ls test/baselines/*.in`;
 do
   infile=$(basename $fn)
@@ -9,11 +10,12 @@ do
 
   echo -n "testing '"
   echo -n $infile
-  echo "'"
+  echo -n "'... "
 
   if [ -e $lexfile ]
   then
       diff <(stack exec compiler \-\- --lex $fn) $lexfile || (>&2 echo "Failed lexing"; exit 1)
+      echo "Passed!"
   else
       >&2 echo "No such file '${lexfile}'"
       exit 1;
@@ -32,6 +34,24 @@ do
       diff <(stack exec compiler \-\- $fn) $outfile || (>&2 echo "Failed evaluation"; exit 1)
   else
       >&2 echo "No such file '${outfile}'"
+      exit 1;
+  fi
+done
+
+echo -e "\nTesting expected failures:\n"
+for fn in `ls test/baselines/*.fail`;
+do
+  infile=$(basename $fn)
+  echo -n "testing '"
+  echo -n $infile
+  echo -n "'... "
+
+  stack exec compiler \-\- $fn &> /dev/null
+  if [ $? == 1 ]
+  then
+      echo "Passed!"
+  else
+      >&2 echo "Failed!"
       exit 1;
   fi
 done
