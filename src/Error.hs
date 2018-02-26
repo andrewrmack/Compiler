fec536db513fe425561ba15b0eae6c3861ec8377
Andrew Mack <andrewrmack@gmail.com>
2018-02-26 15:08:40 -0600

    Change up error handling a little

--- src/Error.hs
+++ src/Error.hs
@@ -11,9 +11,13 @@ module Error
 import Control.Lens
 import Control.Monad.State
 import Location (Location(..))
+import System.Exit
 import System.IO
+import System.IO.Unsafe
 
 type Compiler a = State CompilerState a
+
+data MessageType = Error | Warning deriving Show
 
 newtype CompilerState = CompilerState {
     _warnings :: [String]
@@ -25,17 +29,22 @@ emptyState = CompilerState []
 makeLenses ''CompilerState
 
 locatedError :: Location -> String -> a
-locatedError l msg = errorWithoutStackTrace $ locatedMessage l msg
+locatedError l msg = fatalError $ locatedMessage Error l msg
+
+fatalError :: String -> a
+fatalError msg = unsafePerformIO $ do
+  hPutStrLn stderr msg
+  exitFailure
 
 logWarning :: Location -> String -> Compiler ()
-logWarning l msg = modify (warnings %~ (locatedMessage l msg :))
+logWarning l msg = modify (warnings %~ (locatedMessage Warning l msg :))
 
-locatedMessage :: Location -> String -> String
-locatedMessage (Location r c) msg =
-  msg ++ " at (line " ++ show r ++ ", column " ++ show c ++ ")"
+locatedMessage :: MessageType -> Location -> String -> String
+locatedMessage t (Location r c) msg =
+  show t ++ ": " ++ msg ++ " at (line " ++ show r ++ ", column " ++ show c ++ ")"
 
 getWarnings :: Compiler a -> [String]
-getWarnings c = _warnings $ execState c emptyState
+getWarnings c = reverse . _warnings $ execState c emptyState
 
 showWarnings :: [String] -> IO ()
 showWarnings = mapM_ (hPutStrLn stderr)
